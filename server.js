adf5eb4c62051c889f32cf8a306d63eefc84a9e9
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-04-26 22:11:14 -0300

    Server application created

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express');
+var logger = require('morgan');
+var app = express();
+
+// log requests
+app.use(logger('dev'));
+
+app.get('/', function(req, res){
+  res.redirect('/app');
+});
+
+app.use('/app', express.static(__dirname + '/app'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+
+app.listen(3000);
+console.log('listening on port 3000');
