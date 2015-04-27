80cee2aea8367788be22e2a80921affd5ace1924
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-04-26 21:07:54 -0300

    Configured Grunt's bower integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,20 @@
-// Project configuration.
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json')
-});
+'use strict';
 
-// Load the plugin that provides the "uglify" task.
-grunt.loadNpmTasks('grunt-bower-requirejs');
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bowerRequirejs: {
+      target: {
+        rjsConfig: 'app/scripts/main.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-bower-requirejs');
+
+  grunt.registerTask('default', ['bowerRequirejs']);
+
+};
---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+
+  },
+  packages: [
+
+  ]
+});
