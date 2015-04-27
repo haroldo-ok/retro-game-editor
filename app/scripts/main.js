e5997a4ea47e24fd0cc1e1fde122c283f7a50a1f
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-04-27 20:47:26 -0300

    localForage installed.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,12 +6,23 @@ require.config({
     jquery: "../../bower_components/jquery/dist/jquery",
     requirejs: "../../bower_components/requirejs/require",
     backbone: "../../bower_components/backbone/backbone",
-    underscore: "../../bower_components/underscore/underscore"
+    underscore: "../../bower_components/underscore/underscore",
+    localforage: "../../bower_components/localforage/dist/localforage",
+    "localforage-backbone": "../../bower_components/localforage-backbone/dist/localforage.backbone.min"
   },
   packages: [
 
   ]
 });
 
-require(["jquery", "backbone", "underscore"], function($, Backbone, _) {
+require(["jquery", "backbone", "underscore", "localforage", "localforage-backbone"], function($, Backbone, _) {
+  // Just for a quick-and-dirty test. TODO: Remove this.
+  window.MyModel = Backbone.Model.extend({
+      sync: Backbone.localforage.sync('MyModel')
+  });
+
+  window.MyCollection = Backbone.Collection.extend({
+      model: MyModel,
+      sync: Backbone.localforage.sync('MyCollection')
+  });
 });
