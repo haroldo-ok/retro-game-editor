e05c919414c771319a04d56495502272bf14c861
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-04-28 20:33:26 -0300

    Auto-add models to collection
    Assured that models, when saved, will be automatically added to their respective
    collections.

--- app/scripts/model/main.js
+++ app/scripts/model/main.js
@@ -1,13 +1,23 @@
 define(["backbone", "localforage-backbone"], function(Backbone){
   return function(name, options){
     var Model = Backbone.Model.extend({
+        save: function(attributes, options){
+          // Adds it to the main collection
+          if (this.isNew()) {
+            Model.objects.add(this);
+          }
+          // Does the actual saving
+          Backbone.Model.prototype.save.apply(this, arguments);
+        },
         sync: Backbone.localforage.sync(name)
     });
 
-    Model.objects = Backbone.Collection.extend({
+    var ModelCollection = Backbone.Collection.extend({
         model: Model,
         sync: Backbone.localforage.sync(name)
     });
+
+    Model.objects = new ModelCollection();
 
     return Model;
   }
