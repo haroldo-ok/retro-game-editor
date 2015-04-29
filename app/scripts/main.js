6a1750e540ea98c26e3a55e68e01c26a238b5f66
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-04-29 20:54:53 -0300

    Dockspawn added to project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 require.config({
   shim: {
-
+    dockspawn: {
+      exports: 'dockspawn'
+    }
   },
   paths: {
     jquery: "../../bower_components/jquery/dist/jquery",
@@ -8,7 +10,8 @@ require.config({
     backbone: "../../bower_components/backbone/backbone",
     underscore: "../../bower_components/underscore/underscore",
     localforage: "../../bower_components/localforage/dist/localforage",
-    "localforage-backbone": "../../bower_components/localforage-backbone/dist/localforage.backbone.min"
+    "localforage-backbone": "../../bower_components/localforage-backbone/dist/localforage.backbone.min",
+    dockspawn: "../../bower_components/dock-spawn/js/out/js/dockspawn"
   },
   packages: [
     "model"
