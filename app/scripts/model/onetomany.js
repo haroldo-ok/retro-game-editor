032a07b4f00234a22ef909e81fd83e280c40439e
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-05-01 16:45:23 -0300

    OnetoMany.fetch() adapted to use promises

--- app/scripts/model/onetomany.js
+++ app/scripts/model/onetomany.js
@@ -1,4 +1,4 @@
-define(["backbone", "underscore"], function(Backbone, _){
+define(["backbone", "underscore", "q"], function(Backbone, _, Q){
   function OneToMany(one, many, ref){
     _.extend(this, Backbone.Events);
 
@@ -31,17 +31,22 @@ define(["backbone", "underscore"], function(Backbone, _){
   OneToMany.prototype.fetch = function() {
     that = this;
     this._items = [];
-    this._many.forEach(function(Model){
-      Model.objects.fetch({
-          success: function(){
-            var filter = {};
-            filter[that._ref] = that._one.id;
 
-            var values = Model.objects.where(filter);
-            that._items = that._items.concat(values);
-          }
+    var promises = this._many.map(function(Model){
+      return Model.objects.fetch();
+    });
+
+    return Q.all(promises)
+      .then(function(results){
+        results.forEach(function(collection){
+          var filter = {};
+          filter[that._ref] = that._one.id;
+
+          var values = collection.where(filter);
+          that._items = that._items.concat(values);
+        });
+        return that._items;
       });
-    })
   }
 
   return OneToMany;
