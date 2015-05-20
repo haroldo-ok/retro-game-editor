2b8f43e991fae9a6c0f16fb85a3e8a2811246930
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-05-19 22:57:40 -0300

    Added tileset select

--- app/internal-apps/tiny-map-editor/tileEditor.js
+++ app/internal-apps/tiny-map-editor/tileEditor.js
@@ -1,255 +1,283 @@
-var tinyMapEditor = (function() {
-    var win = window,
-        doc = document,
-        pal = doc.getElementById('palette').getContext('2d'),
-        map = doc.getElementById('tileEditor').getContext('2d'),
-        width = 16,
-        height = 12,
-        tileSize = 16,
-        srcTile = 0,
-        sprite = new Image(),
-        tiles, // used for demo, not *really* needed atm
-        alpha,
+top.require(["jquery", "model", "model/project", "view/util/query-string"],
+function($, model, Project, queryString){
 
-        selectedZoom = 4,
-        mapZoom = 2,
+  var tinyMapEditor = (function() {
+      var win = window,
+          doc = document,
+          pal = doc.getElementById('palette').getContext('2d'),
+          map = doc.getElementById('tileEditor').getContext('2d'),
+          width = 16,
+          height = 12,
+          tileSize = 16,
+          srcTile = 0,
+          sprite = new Image(),
+          tiles, // used for demo, not *really* needed atm
+          alpha,
 
-        player,
-        draw,
-        build = doc.getElementById('build'),
-        test = doc.getElementById('test');
+          selectedZoom = 4,
+          mapZoom = 2,
 
-    var app = {
-        getTile : function(e) {
-            if (e.target.nodeName === 'CANVAS') {
-                var targetId = e.target.id;
-                var zoom = targetId == 'tileEditor' ? mapZoom : 1;
+          player,
+          draw,
+          build = doc.getElementById('build'),
+          test = doc.getElementById('test'),
 
-                var row = Math.floor(e.layerX / tileSize / zoom) | 0,
-                    col = Math.floor(e.layerY / tileSize / zoom) | 0;
+          $doc = $(doc);
 
-                if (e.target.id === 'palette') srcTile = { row : row, col : col };
+      var app = {
+          getTile : function(e) {
+              if (e.target.nodeName === 'CANVAS') {
+                  var targetId = e.target.id;
+                  var zoom = targetId == 'tileEditor' ? mapZoom : 1;
 
-                return { row : row, col : col };
-            }
-        },
+                  var row = Math.floor(e.layerX / tileSize / zoom) | 0,
+                      col = Math.floor(e.layerY / tileSize / zoom) | 0;
 
-        setTile : function(e) {
-            var destTile;
+                  if (e.target.id === 'palette') srcTile = { row : row, col : col };
 
-            if (e.target.id === 'tileEditor' && srcTile && !draw) {
-                destTile = this.getTile(e);
-                map.clearRect(destTile.row * tileSize, destTile.col * tileSize, tileSize, tileSize);
-                map.drawImage(sprite, srcTile.row * tileSize, srcTile.col * tileSize, tileSize, tileSize, destTile.row * tileSize, destTile.col * tileSize, tileSize, tileSize);
-            }
-        },
+                  return { row : row, col : col };
+              }
+          },
 
-        drawTool : function() {
-            var rect = doc.createElement('canvas'),
-                ctx = rect.getContext('2d'),
-                eraser = function() {
-                    ctx.fillStyle = 'red';
-                    ctx.fillRect(0, 0, tileSize, tileSize);
-                    ctx.fillStyle = 'white';
-                    ctx.fillRect(2, 2, tileSize - 4, tileSize - 4);
-                    ctx.strokeStyle = 'red';
-                    ctx.lineWidth = 2;
-                    ctx.moveTo(tileSize, 0);
-                    ctx.lineTo(0, tileSize);
-                    ctx.stroke();
-                };
+          setTile : function(e) {
+              var destTile;
 
-            rect.width = rect.height = tileSize;
-            rect.style.width = (rect.width * selectedZoom) + 'px';
-            doc.getElementById('selected').appendChild(rect);
-            eraser();
+              if (e.target.id === 'tileEditor' && srcTile && !draw) {
+                  destTile = this.getTile(e);
+                  map.clearRect(destTile.row * tileSize, destTile.col * tileSize, tileSize, tileSize);
+                  map.drawImage(sprite, srcTile.row * tileSize, srcTile.col * tileSize, tileSize, tileSize, destTile.row * tileSize, destTile.col * tileSize, tileSize, tileSize);
+              }
+          },
 
-            this.drawTool = function() {
-                rect.width = tileSize;
-                srcTile ? ctx.drawImage(sprite, srcTile.row * tileSize, srcTile.col * tileSize, tileSize, tileSize, 0, 0, tileSize, tileSize) : eraser();
-            };
-        },
+          drawTool : function() {
+              var rect = doc.createElement('canvas'),
+                  ctx = rect.getContext('2d'),
+                  eraser = function() {
+                      ctx.fillStyle = 'red';
+                      ctx.fillRect(0, 0, tileSize, tileSize);
+                      ctx.fillStyle = 'white';
+                      ctx.fillRect(2, 2, tileSize - 4, tileSize - 4);
+                      ctx.strokeStyle = 'red';
+                      ctx.lineWidth = 2;
+                      ctx.moveTo(tileSize, 0);
+                      ctx.lineTo(0, tileSize);
+                      ctx.stroke();
+                  };
 
-        eraseTile : function(e) {
-            var destTile;
-            if (!draw) {
-                if (e.target.id === 'erase' && srcTile) {
-                    srcTile = 0;
-                } else if (e.target.id === 'tileEditor' && !srcTile) {
-                    destTile = this.getTile(e);
-                    map.clearRect(destTile.row * tileSize, destTile.col * tileSize, tileSize, tileSize);
-                }
-            }
-        },
+              rect.width = rect.height = tileSize;
+              rect.style.width = (rect.width * selectedZoom) + 'px';
+              doc.getElementById('selected').appendChild(rect);
+              eraser();
 
-        drawMap : function() {
-            var i, j, invert = document.getElementById('invert').checked ? 0 : 1;
+              this.drawTool = function() {
+                  rect.width = tileSize;
+                  srcTile ? ctx.drawImage(sprite, srcTile.row * tileSize, srcTile.col * tileSize, tileSize, tileSize, 0, 0, tileSize, tileSize) : eraser();
+              };
+          },
 
-            map.fillStyle = 'black';
-            for (i = 0; i < width; i++) {
-                for (j = 0; j < height; j++) {
-                    if (alpha[i][j] === invert) {
-                        map.fillRect(i * tileSize, j * tileSize, tileSize, tileSize);
-                    } else if (typeof alpha[i][j] === 'object') {
-                        // map.putImageData(tiles[i][j], i * tileSize, j * tileSize); // temp fix to colour collision layer black
-                    }
-                }
-            }
-        },
+          eraseTile : function(e) {
+              var destTile;
+              if (!draw) {
+                  if (e.target.id === 'erase' && srcTile) {
+                      srcTile = 0;
+                  } else if (e.target.id === 'tileEditor' && !srcTile) {
+                      destTile = this.getTile(e);
+                      map.clearRect(destTile.row * tileSize, destTile.col * tileSize, tileSize, tileSize);
+                  }
+              }
+          },
 
-        clearMap : function(e) {
-            if (e.target.id === 'clear') {
-                map.clearRect(0, 0, map.canvas.width, map.canvas.height);
-                this.destroy();
-                build.disabled = false;
-            }
-        },
+          drawMap : function() {
+              var i, j, invert = document.getElementById('invert').checked ? 0 : 1;
 
-        buildMap : function(e) {
-            if (e.target.id === 'build') {
-                var obj = {},
-                    pixels,
-                    len,
-                    x, y, z;
+              map.fillStyle = 'black';
+              for (i = 0; i < width; i++) {
+                  for (j = 0; j < height; j++) {
+                      if (alpha[i][j] === invert) {
+                          map.fillRect(i * tileSize, j * tileSize, tileSize, tileSize);
+                      } else if (typeof alpha[i][j] === 'object') {
+                          // map.putImageData(tiles[i][j], i * tileSize, j * tileSize); // temp fix to colour collision layer black
+                      }
+                  }
+              }
+          },
 
-                tiles = []; // graphical tiles (not currently needed, can be used to create standard tile map)
-                alpha = []; // collision map
+          clearMap : function(e) {
+              if (e.target.id === 'clear') {
+                  map.clearRect(0, 0, map.canvas.width, map.canvas.height);
+                  this.destroy();
+                  build.disabled = false;
+              }
+          },
 
-                for (x = 0; x < width; x++) { // tiles across
-                    tiles[x] = [];
-                    alpha[x] = [];
+          buildMap : function(e) {
+              if (e.target.id === 'build') {
+                  var obj = {},
+                      pixels,
+                      len,
+                      x, y, z;
 
-                    for (y = 0; y < height; y++) { // tiles down
-                        pixels = map.getImageData(x * tileSize, y * tileSize, tileSize, tileSize);
-                        len = pixels.data.length;
+                  tiles = []; // graphical tiles (not currently needed, can be used to create standard tile map)
+                  alpha = []; // collision map
 
-                        tiles[x][y] = pixels; // store ALL tile data
-                        alpha[x][y] = [];
+                  for (x = 0; x < width; x++) { // tiles across
+                      tiles[x] = [];
+                      alpha[x] = [];
 
-                        for (z = 0; z < len; z += 4) {
-                            pixels.data[z] = 0;
-                            pixels.data[z + 1] = 0;
-                            pixels.data[z + 2] = 0;
-                            alpha[x][y][z / 4] = pixels.data[z + 3]; // store alpha data only
-                        }
+                      for (y = 0; y < height; y++) { // tiles down
+                          pixels = map.getImageData(x * tileSize, y * tileSize, tileSize, tileSize);
+                          len = pixels.data.length;
 
-                        if (alpha[x][y].indexOf(0) === -1) { // solid tile
-                            alpha[x][y] = 1;
-                        } else if (alpha[x][y].indexOf(255) === -1) { // transparent tile
-                            alpha[x][y] = 0;
-                        } else { // partial alpha, build pixel map
-                            alpha[x][y] = this.sortPartial(alpha[x][y]);
-                            tiles[x][y] = pixels; // (temporarily) used for drawing map
-                        }
-                    }
-                }
+                          tiles[x][y] = pixels; // store ALL tile data
+                          alpha[x][y] = [];
 
-                this.outputJSON();
-                this.drawMap();
-            }
-        },
+                          for (z = 0; z < len; z += 4) {
+                              pixels.data[z] = 0;
+                              pixels.data[z + 1] = 0;
+                              pixels.data[z + 2] = 0;
+                              alpha[x][y][z / 4] = pixels.data[z + 3]; // store alpha data only
+                          }
 
-        sortPartial : function(arr) {
-            var len = arr.length,
-                temp = [],
-                i, j;
+                          if (alpha[x][y].indexOf(0) === -1) { // solid tile
+                              alpha[x][y] = 1;
+                          } else if (alpha[x][y].indexOf(255) === -1) { // transparent tile
+                              alpha[x][y] = 0;
+                          } else { // partial alpha, build pixel map
+                              alpha[x][y] = this.sortPartial(alpha[x][y]);
+                              tiles[x][y] = pixels; // (temporarily) used for drawing map
+                          }
+                      }
+                  }
 
-            for (i = 0; i < tileSize; i++) {
-                temp[i] = [];
-                for (j = 0; j < len; j++) {
-                    if (j % tileSize === j) {
-                        temp[i][j] = arr[j * tileSize + i];
-                    }
-                }
-                temp[i] = temp[i].indexOf(255);
-            }
-            return temp;
-        },
+                  this.outputJSON();
+                  this.drawMap();
+              }
+          },
 
-        outputJSON : function() {
-            var output = '',
-                invert = document.getElementById('invert').checked;
+          sortPartial : function(arr) {
+              var len = arr.length,
+                  temp = [],
+                  i, j;
 
-            if (invert) {
-                alpha.forEach(function(arr) {
-                    arr.forEach(function(item, index) {
-                        // using bitwise not to flip values
-                        if (typeof item === 'number') arr[index] = Math.abs(~-item);
-                    });
-                });
-            }
+              for (i = 0; i < tileSize; i++) {
+                  temp[i] = [];
+                  for (j = 0; j < len; j++) {
+                      if (j % tileSize === j) {
+                          temp[i][j] = arr[j * tileSize + i];
+                      }
+                  }
+                  temp[i] = temp[i].indexOf(255);
+              }
+              return temp;
+          },
 
-            // output = (output.split('],'));
-            // output = output.concat('],');
+          outputJSON : function() {
+              var output = '',
+                  invert = document.getElementById('invert').checked;
 
-            output = JSON.stringify(alpha);
-            doc.getElementsByTagName('textarea')[0].value = output;
-        },
+              if (invert) {
+                  alpha.forEach(function(arr) {
+                      arr.forEach(function(item, index) {
+                          // using bitwise not to flip values
+                          if (typeof item === 'number') arr[index] = Math.abs(~-item);
+                      });
+                  });
+              }
 
-        bindEvents : function() {
-            var _this = this;
+              // output = (output.split('],'));
+              // output = output.concat('],');
 
+              output = JSON.stringify(alpha);
+              doc.getElementsByTagName('textarea')[0].value = output;
+          },
 
-            /**
-             * Window events
-             */
+          loadMap: function() {
+            var query = queryString.parse(location.search);
+            var Model = model.byName(query.entity);
+            this.mapEntity = Model.objects.get(query.entityId);
+            this.project = Project.objects.get(this.mapEntity.get('projectId'));
+          },
 
-            win.addEventListener('click', function(e) {
-                _this.setTile(e);
-                _this.getTile(e);
-                _this.eraseTile(e);
-                _this.drawTool();
-                _this.clearMap(e);
-                _this.buildMap(e);
-            }, false);
+          populateTilesets: function() {
+            var $select = $doc.find('#tileSets');
 
+            $select.empty();
+            this.project.resources.ofType('TileSet').forEach(function(tileSet){
+              $select.append($('<option>')
+                .attr('value', tileSet.get('id'))
+                .text(tileSet.get('name') || '**unnamed**'));
+            });
+          },
 
-            /**
-             * Image load event
-             */
-
-            sprite.addEventListener('load', function() {
-                pal.canvas.width = this.width;
-                pal.canvas.height = this.height;
-                pal.drawImage(this, 0, 0);
-            }, false);
+          bindEvents : function() {
+              var _this = this;
 
 
-            /**
-             * Input change events
-             */
+              /**
+               * Window events
+               */
 
-            document.getElementById('width').addEventListener('change', function() {
-                width = +this.value;
-                _this.destroy();
-                _this.init();
-            }, false);
-
-            document.getElementById('height').addEventListener('change', function() {
-                height = +this.value;
-                _this.destroy();
-                _this.init();
-            }, false);
-        },
-
-        init : function() {
-            sprite.src = 'assets/tilemap_32a.png';
-            map.canvas.width = width * tileSize;
-            map.canvas.height = height * tileSize;
-            map.canvas.style.width = (map.canvas.width * mapZoom) + 'px';
-            this.drawTool();
-        },
-
-        destroy : function() {
-            clearInterval(draw);
-            alpha = [];
-        }
-    };
+              win.addEventListener('click', function(e) {
+                  _this.setTile(e);
+                  _this.getTile(e);
+                  _this.eraseTile(e);
+                  _this.drawTool();
+                  _this.clearMap(e);
+                  _this.buildMap(e);
+              }, false);
 
 
+              /**
+               * Image load event
+               */
 
-    app.bindEvents();
-    app.init();
-    return app;
+              sprite.addEventListener('load', function() {
+                  pal.canvas.width = this.width;
+                  pal.canvas.height = this.height;
+                  pal.drawImage(this, 0, 0);
+              }, false);
 
-})();
+
+              /**
+               * Input change events
+               */
+
+              document.getElementById('width').addEventListener('change', function() {
+                  width = +this.value;
+                  _this.destroy();
+                  _this.init();
+              }, false);
+
+              document.getElementById('height').addEventListener('change', function() {
+                  height = +this.value;
+                  _this.destroy();
+                  _this.init();
+              }, false);
+          },
+
+          init : function() {
+              this.loadMap();
+              this.populateTilesets();
+
+              sprite.src = 'assets/tilemap_32a.png';
+              map.canvas.width = width * tileSize;
+              map.canvas.height = height * tileSize;
+              map.canvas.style.width = (map.canvas.width * mapZoom) + 'px';
+              this.drawTool();
+          },
+
+          destroy : function() {
+              clearInterval(draw);
+              alpha = [];
+          }
+      };
+
+
+
+      app.bindEvents();
+      app.init();
+      return app;
+
+  })();
+
+});
